d57df1763ee8664bf2d25b942f564834c9222089
Christine Kirk <christinedianekirk@gmail.com>
2026-06-26 01:48:07 -0700

    Add LoginScreen to AppNavigator and set initial route

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,13 +1,15 @@
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 import HomeScreen from "../screens/HomeScreen";
+import LoginScreen from "../screens/LoginScreen";
 
 const Stack = createStackNavigator();
 
 export default function AppNavigator() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Inicio" component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
