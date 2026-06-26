c25b5cfcf2175cf7cdebeb7dd3bce517136b6981
Christine Kirk <christinedianekirk@gmail.com>
2026-06-26 02:15:11 -0700

    Add FamiliaScreen component for family information

---
+++ FamiliaScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function FamiliaScreen() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Familia</h1>
+      <p>Aquí podrás ver información y herramientas para tu familia.</p>
+    </div>
+  );
+}
