15e8bf0938492bfd8761d0b971c347fbec6984f4
Christine Kirk <christinedianekirk@gmail.com>
2026-06-26 01:45:58 -0700

    Add LoginScreen component with welcome message

---
+++ screens/Logingscreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function LoginScreen() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Iniciar Sesión</h1>
+      <p>Bienvenida. Aquí podrás entrar a tu cuenta familiar.</p>
+    </div>
+  );
+}
