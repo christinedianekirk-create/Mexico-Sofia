f2aa22a809e8f42b3a569ae200ecba52be4658b6
Christine Kirk <christinedianekirk@gmail.com>
2026-06-26 02:10:50 -0700

    Add MenuScreen to AppNavigator

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -2,6 +2,7 @@ import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 import HomeScreen from "../screens/HomeScreen";
 import LoginScreen from "../screens/LoginScreen";
+import MenuScreen from "../screens/MenuScreen";
 
 const Stack = createStackNavigator();
 
@@ -11,6 +12,7 @@ export default function AppNavigator() {
       <Stack.Navigator initialRouteName="Login">
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Inicio" component={HomeScreen} />
+        <Stack.Screen name="Menú" component={MenuScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
