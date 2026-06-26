840cc779d0140caaa61763ee3c795723dda47721
Christine Kirk <christinedianekirk@gmail.com>
2026-06-26 01:43:40 -0700

    Create HomeScreen component for welcome message

---
+++ screens/homescreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function HomeScreen() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Bienvenida a MÉXICO–SOFÍA</h1>
+      <p>Tu app familiar ya está funcionando.</p>
+    </div>
+  );
+}
