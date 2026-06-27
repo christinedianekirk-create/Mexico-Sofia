2c9f1da3a47ab79212a84e973e6ac9f928945a30
Christine Kirk <christinedianekirk@gmail.com>
2026-06-26 18:05:07 -0700

    Add HomeScreen component with welcome message

--- screens/screens/homescreen.js
+++ screens/screens/homescreen.js
@@ -1,1 +1,10 @@
+import React from "react";
 
+export default function HomeScreen() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Inicio</h1>
+      <p>Bienvenida a la pantalla principal de SOFÍA.</p>
+    </div>
+  );
+}
